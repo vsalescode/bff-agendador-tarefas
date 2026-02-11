4c994d93af01f92dce1be12a7e2fa06f4110464a
Victor <vssfullstack@gmail.com>
2026-02-11 14:34:45 -0300

    esqueleto incial do projeto

---
+++ src/main/java/com/vssfullstack/bff_agendador_tarefas/BffAgendadorApplication.java
@@ -0,0 +1,13 @@
+package com.vssfullstack.bff_agendador_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vssfullstack/bff_agendador_tarefas/BffAgendadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vssfullstack.bff_agendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
