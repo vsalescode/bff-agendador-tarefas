c77b25fa02968bdc3acee15e278d27c7369cdab3
Victor <vssfullstack@gmail.com>
2026-02-12 14:58:17 -0300

    Adicionado metodo cron para verificação de status da tarefas

--- src/main/java/com/vssfullstack/bff_agendador_tarefas/infrastructure/client/EmailClient.java
+++ src/main/java/com/vssfullstack/bff_agendador_tarefas/infrastructure/client/EmailClient.java
@@ -4,10 +4,11 @@ package com.vssfullstack.bff_agendador_tarefas.infrastructure.client;
 
 import com.vssfullstack.bff_agendador_tarefas.business.dto.out.TarefasResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "notificacao", url = "${notificacao.url}")
 public interface EmailClient {
-
+    @PostMapping
     void enviarEmail(@RequestBody TarefasResponseDTO tarefasDTO);
 }
